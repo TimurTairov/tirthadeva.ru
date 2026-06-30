4440c5fc74e60381ef25122e182260647c29be63
Timur Tairov <tairovtk@gmail.com>
2026-06-30 19:23:17 +0300

    commit 1.75

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Тиртхадэва | йога, медитация, адвайта.",
+  title: "Тиртхадева | йога, медитация, адвайта.",
   description: "Хатха йога, кундалини йога, медитация, йога онлайн",
 };
 
